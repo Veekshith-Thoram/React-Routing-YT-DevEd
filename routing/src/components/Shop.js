41b78ad949235050b45424b3ec240df7331a321c
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-06-21 22:36:20 +0530

    Update Shop.js

--- routing/src/components/Shop.js
+++ routing/src/components/Shop.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from "react";
 // import './App.css';
 import {Link} from "react-router-dom"
-import axios from "axios"
+// import axios from "axios"
 function Shop() {
     const [title, setTitle]= useState([])
     useEffect(() => {
